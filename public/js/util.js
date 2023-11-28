046205da0279f5e35021f1f6261abb9c6bb0a97f
Andri Priyadi <dripriyadi@gmail.com>
2023-11-28 13:25:54 +0700

    fixed confirmation

--- public/js/util.js
+++ public/js/util.js
@@ -4,3 +4,12 @@ function confirmation(name) {
         return true
     } else return false
 }
+function opendialog(id, name){
+    document.getElementById('deleteConfirm').style.display = 'block'
+    document.getElementById('dialog').innerHTML = `Are you sure you want to delete '${name}'?`
+    document.getElementById('confirmed').setAttribute("href" , `/delete/${id}`)
+}
+
+function closeDialog() {
+    document.getElementById('deleteConfirm').style.display = 'none';
+}
